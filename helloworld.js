cab76e6d6d3317033ad63745410f23f88817461f
Sainadh4108 <64917795+Sainadh4108@users.noreply.github.com>
2022-11-15 13:00:19 -0600

    Added initial CV website files

---
+++ helloworld.js
@@ -0,0 +1,7 @@
+let js ="Amazing";
+if(js ="Amazing") alert("Java script is fun");
+console.log(10+20+430);
+console.log('Jonas');
+console.log("Hello Sainadh");
+let firstname = "Sainadh Tirumalasetty";
+console.log(firstname);
